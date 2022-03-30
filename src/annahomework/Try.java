94c79354e20a6a17ea9ae4f8670756814b09d9ac
Anna Gishyan <anna.gishyang@gmail.com>
2022-03-30 23:56:25 +0400

    try

---
+++ src/annahomework/Try.java
@@ -0,0 +1,22 @@
+package annahomework;
+
+public class Try {
+    public static void max(int x[]) {
+        int max = x[0];
+        for (int i = 0; i < x.length; i++) {
+            if (x[i] > max)
+                max = x[i];
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+//        // array's max and min
+        int[] arr = {3, 4, 5};
+        System.out.println();
+//        System.out.println(Arrays.stream(arr).max());
+
+
+    }
+}
